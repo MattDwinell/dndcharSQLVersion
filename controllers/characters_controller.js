3a48c36aeba1caf9b44d6e34036f384c34f30ed7
Matt D <dwinell.matt@gmail.com>
2019-06-23 12:29:45 -0400

    calls to the database for a particular character now populate that character stats on the page

--- controllers/characters_controller.js
+++ controllers/characters_controller.js
@@ -1,42 +1,59 @@
 var Characters = require("../models/character.js");
 var Users = require("../models/users.js");
-module.exports = function(app){
-    app.get("/api/characters", (req, res)=>{
-        Characters.findAll().then((result)=>{
+module.exports = function (app) {
+
+    //returns all characters in the characters table
+    app.get("/api/characters", (req, res) => {
+        Characters.findAll().then((result) => {
             return res.json(result);
         })
     })
-app.post("/api/characters", (req,res)=>{
-    console.log('this is what is sent to the post request' +JSON.stringify(req.body));
-    let character = req.body;
-    console.log(character);
-    console.log(character.name);
-Characters.create({
-    userEmail: character.userEmail,
-    charName: character.name,
-    strength: character.str,
-    dexterity: character.dex,
-    constitution: character.con,
-    intelligence: character.int,
-    wisdom: character.wis,
-    charisma: character.cha,
-    armor_class: character.ac,
-    speed: character.spd,
-    hitpoints: character.hp,
-    initiative: character.init,
-    charClass: character.raceClass,
-    charLevel: character.level,
-    charAlignment: character.alignment,
-    charBackground: character.background,
-    charPersonality: character.personality,
-    charInventory: character.inventory
-},
-{
-    timestamps:false,
-    freezeTableName: true
-}).then((results)=>{
-    res.json(results);
-})
-})
+    //returns a character with the name specified by the character element the user clicks on in viewer.html
+    app.get("/api/characters/:id", (req, res)=>{
+        let searchId = req.params.id
+        console.log('in api character search');
+        console.log(searchId);
+        Characters.findAll({
+            where:{
+                id: searchId
+            }
+        }).then((result)=>{
+            return res.json(result);
+        })
+    })
+
+//adds a new character to the table
+    app.post("/api/characters", (req, res) => {
+        console.log('this is what is sent to the post request' + JSON.stringify(req.body));
+        let character = req.body;
+        console.log(character);
+        console.log(character.name);
+        Characters.create({
+            userEmail: character.userEmail,
+            charName: character.name,
+            strength: character.str,
+            dexterity: character.dex,
+            constitution: character.con,
+            intelligence: character.int,
+            wisdom: character.wis,
+            charisma: character.cha,
+            armor_class: character.ac,
+            speed: character.spd,
+            hitpoints: character.hp,
+            initiative: character.init,
+            charClass: character.raceClass,
+            charLevel: character.level,
+            charAlignment: character.alignment,
+            charBackground: character.background,
+            charPersonality: character.personality,
+            charInventory: character.inventory
+        },
+            {
+                timestamps: false,
+                freezeTableName: true
+            }).then((results) => {
+                res.json(results);
+            })
+    })
 
 }
--- public/assets/javascript/viewer.js
+++ public/assets/javascript/viewer.js
@@ -9,8 +9,9 @@ function charactersCall(){
        let characters = data;
        for (let i=0; i<data.length; i++){
            let name = data[i].charName;
+           let charId = data[i].id
            console.log(name);
-           let tempChar = $("<div>").text(name).attr("character-name", name).addClass("character cyan darken-4");
+           let tempChar = $("<div>").text(name).attr({"character-name": name, "char-id": charId}).addClass("character cyan darken-4");
            $("#character-buttons").append(tempChar);
        }
        console.log(characters);
@@ -19,8 +20,48 @@ function charactersCall(){
 
 $(document).on("click", ".character", findCharacter);
 function findCharacter(){
-    let queryTerm = $(this).attr("character-name");
-    console.log(queryTerm);
+    let queryTerm = $(this).attr("char-id");
+    $.get("/api/characters/" + queryTerm, (data)=>{
+        console.log(data);
+        let {strength, dexterity, constitution, intelligence, wisdom, charisma, armor_class, speed, hitpoints, initiative, charClass, charLevel,charAlignment, charBackground, charPersonality, charInventory, charName} = data[0];
+        let str = $("<span>").text(strength).addClass("cyan darken-4");
+        let dex = $("<span>").text(dexterity).addClass("cyan darken-4");
+        let con = $("<span>").text(constitution).addClass("cyan darken-4");
+        let int = $("<span>").text(intelligence).addClass("cyan darken-4");
+        let wis = $("<span>").text(wisdom).addClass("cyan darken-4");
+        let cha = $("<span>").text(charisma).addClass("cyan darken-4");
+        let ac = $("<span>").text(armor_class).addClass("cyan darken-4");
+        let spd = $("<span>").text(speed).addClass("cyan darken-4");
+        let hp = $("<span>").text(hitpoints).addClass("cyan darken-4");
+        let init = $("<span>").text(initiative).addClass("cyan darken-4");
+        let characterClass = $("<span>").text(charClass).addClass("cyan darken-4");
+        let level = $("<span>").text(charLevel).addClass("cyan darken-4");
+        let alignment = $("<span>").text(charAlignment).addClass("cyan darken-4");
+        let background = $("<span>").text(charBackground).addClass("cyan darken-4");
+        let personality = $("<span>").text(charPersonality).addClass("cyan darken-4");
+        let inventory = $("<span>").text(charInventory).addClass("cyan darken-4");
+        let displayName = $("<span>").text(charName).addClass("cyan darken-4");
+
+
+        $("#char-str").empty().append(str);
+        $("#char-dex").empty().append(dex);
+        $("#char-con").empty().append(con);
+        $("#char-int").empty().append(int);
+        $("#char-wis").empty().append(wis);
+        $("#char-cha").empty().append(cha);
+        $("#char-ac").empty().append(ac);
+        $("#char-spd").empty().append(spd);
+        $("#char-hp").empty().append(hp);
+        $("#char-init").empty().append(init);
+        $("#char-race").empty().append(characterClass);
+        $("#char-background").empty().append(background);
+        $("#char-inventory").empty().append(inventory);
+        $("#char-personality").empty().append(personality);
+        $("#char-level").empty().append(level);
+        $("#char-alignment").empty().append(alignment);
+        $("#char-name").empty().append(displayName);
+
+    })
 }
 
 })
