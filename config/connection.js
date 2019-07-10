6ccb8bcae04d9f769d1d4e4e058804cd7a64c912
Matt D <dwinell.matt@gmail.com>
2019-07-09 21:49:50 -0400

    configuring jawsdb

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,18 @@ const jawsdb = process.env.JAWSDB_URL;
 const jawsUsername = process.env.jawsUsername;
 const jawsPassword = process.env.password;
 const jawsDBName = "j9sfrsa6x00mtqvh";
-var sequelize = new Sequelize(jawsDBName, jawsUsername, jawsPassword, {
-  host: jawsdb,
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+
+var sequelize = new Sequelize(jawsdb);
+// var sequelize = new Sequelize(jawsDBName, jawsUsername, jawsPassword, {
+//   host: jawsdb,
+//   port: 3306,
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
 
 // Exports the connection for other files to use
 module.exports = sequelize;
